6ff1621af5607c83617279f5b4dfffeb1c89b4cb
Jetsadakorn <my1471@hotmail.com>
2025-02-03 08:54:22 +0700

    fixing

--- controllers/scripts/user/_create.js
+++ controllers/scripts/user/_create.js
@@ -1,29 +1,38 @@
 'use strict'
 
+const Bcrypt = require ('bcrypt')
+
 module.exports = async (request, reply) => {
+
 
   try {
 
     const { 
       name, 
-      email, 
-      password, 
+      email,
+      password,
       phone, 
       rate_discount, 
       wallet 
     } = request.body
+      
+
+    const salt = Bcrypt.genSaltSync (10)
+    console.log ("salt: ", salt)
+    const passwordhash = Bcrypt.hashSync (password, salt)
+    console.log ("passwordhash: ", passwordhash)
 
 
-    const user = await request.MODELS.User ({ 
+    const user = await request.MODELS.User.create ({ 
       name, 
       email, 
-      password, 
+      passwordhash, 
       phone,
       rate_discount,
       wallet
     })
 
-    await user.save()
+    // await user.save ()
 
     return reply.code(201).send({ message: 'User created', user })
 
